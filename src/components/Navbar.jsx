c1bbb314150570ee659ae543e767ebbb89907f84
sujjwal21 <sujjwal85@gmail.com>
2023-04-12 12:21:25 +0530

    component structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/material"
+import { Link } from "react-router-dom"
+import { logo } from "../utils/constant"
+const Navbar = () => {
+  return (
+    <Stack direction="row" alignItems={"center"} p={2} sx={{position:"sticky",background:"#000",}} >
+
+    </Stack>
+  )
+}
+
+export default Navbar
