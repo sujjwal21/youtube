c8c60211cb01466e981911b15669e2d2f5637606
sujjwal21 <sujjwal85@gmail.com>
2023-04-12 13:53:28 +0530

    navbar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constant";
+
+const Sidebar = () => (
+    <Stack 
+    direction="row"
+    sx={{overflow:"auto",height:{sx:'auto'}}}
+    >
+
+    </Stack>
+)
+
+export default Sidebar;
