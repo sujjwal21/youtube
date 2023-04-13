5e2a9f7b5a4bd9c145599b81e16edb4746a12921
sujjwal21 <sujjwal85@gmail.com>
2023-04-13 22:41:16 +0530

    completed most of functionality

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Stack,Box } from "@mui/material";
 import { ChannelCard, VideoCard } from "./";
 
-const Videos = ({videos}) => {
+const Videos = ({videos,direction}) => {
+  if(!videos?.length) return "Loading..."
   console.log(videos)
-  return <Stack direction="row" flexWrap='wrap' justifyContent='start' gap={2}>
+  return <Stack direction={direction||"row"} flexWrap='wrap' justifyContent='start' gap={2}>
     {videos.map((item,idx)=>(
       <Box key={idx}>
         {item.id.videoId&&<VideoCard video={item}/>}
